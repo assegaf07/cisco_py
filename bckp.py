db914147dec5f1c7ccd3b33942e8b37bb1405dbc
Iqbal Assegaf <iqbal.assegaf@bukalapak.com>
2023-10-01 04:08:40 +0700

    kode yg bener

--- bckp.py
+++ bckp.py
@@ -4,28 +4,30 @@
 # import modules needed and set up ssh connection parameters
 import paramiko
 import datetime
-user = 'andmin'
-secret = 'P@$$w0rd!'
+user = 'admin'
+secret = 'bukalapak'
 port = 22
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
  
 # define variables
 time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
-infilepath = "c:\\Users\\johnk\\Downloads\\"
-outfilepath = "c:\\Users\\johnk\\Downloads\\"
-devicelist = "device-list1.txt"
+infilepath = "/root/tftp"
+outfilepath = "/root/tftp"
+devicelist = "ip.txt"
  
 # open device file
 input_file = open( infilepath + devicelist, "r")
 iplist = input_file.readlines()
 input_file.close()
- 
+
+command = "copy startup-config tftp://192.168.200.102/" + str(ipaddr)+ "-" + time_now
+
 # loop through device list and execute commands
 for ip in iplist:
     ipaddr = ip.strip()
     ssh.connect(hostname=ipaddr, username=user, password=secret, port=port)
-    stdin, stdout, stderr = ssh.exec_command('show run')
+    stdin, stdout, stderr = ssh.exec_command(command)
     list = stdout.readlines()
     outfile = open(outfilepath + ipaddr + "_" + time_now, "w")
     for char in list:
